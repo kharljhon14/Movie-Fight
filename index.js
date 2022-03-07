190d2ce453b6de4bf760376288b1a6a40090d4ed
Karl <enriquezkharl14@gmail.com>
2022-03-08 02:46:33 +0800

    Fetching data using Axios

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log('Hi there!');
+const fetchData = async () => {
+   const response = await axios.get("http://www.omdbapi.com/", {
+      params: {
+         apikey: "d127a146",
+         s: "spiderman",
+      },
+   });
+
+   console.log(response.data)
+};
+
+fetchData();
