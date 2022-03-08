f617201f64c472e9e0c6daeab5539521348b8827
Karl <enriquezkharl14@gmail.com>
2022-03-08 18:22:20 +0800

    Getting search Data

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
-const fetchData = async () => {
+const fetchData = async (searchTerm) => {
    const response = await axios.get("http://www.omdbapi.com/", {
       params: {
          apikey: "d127a146",
-         s: "spiderman",
+         s: searchTerm,
       },
    });
 
-   console.log(response.data)
+   console.log(response.data);
 };
 
-fetchData();
+const input = document.querySelector("input");
+let timeoutId;
+
+const onInput = (evt) => {
+   if (timeoutId) clearTimeout(timeoutId);
+   timeoutId = setTimeout(() => {
+      fetchData(evt.target.value);
+   }, 800);
+};
+
+input.addEventListener("input", onInput);
