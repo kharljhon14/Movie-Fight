69c81622468d2f10d19378d8b69f21371a74ac7a
Karl <enriquezkharl14@gmail.com>
2022-03-08 19:51:27 +0800

    Displaying Search results in DOM

--- index.js
+++ index.js
@@ -6,14 +6,24 @@ const fetchData = async (searchTerm) => {
       },
    });
 
-   console.log(response.data);
+   return response.data.Search;
 };
 
 const input = document.querySelector("input");
 
+const onInput = async (evt) => {
+   const movies = await fetchData(evt.target.value);
 
-const onInput = (evt) => {
-   fetchData(evt.target.value);
+   for (let movie of movies) {
+      const div = document.createElement("div");
+
+      div.innerHTML = `
+         <img src="${movie.Poster}"/>
+         <h1>${movie.Title}</h1>
+      `;
+
+      document.querySelector("#target").appendChild(div)
+   }
 };
 
 input.addEventListener("input", debounce(onInput, 800));
